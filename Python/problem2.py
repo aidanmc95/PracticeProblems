7bf74177238544692ac21c2fff40d952ef737c17
aidanmc95 <aidanmc95@gmail.com>
2020-11-14 21:43:27 -0800

    Solved for neg

--- Python/problem2.py
+++ Python/problem2.py
@@ -1,12 +1,17 @@
-def divide(divided, divisor):
+def divide(dividend, divisor):
+    add = 1
+    if((dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0)):
+        add = -1
     count = 0
-    while (divided > 0):
+    dividend = abs(dividend)
+    divisor = abs(divisor)
+    while (dividend > 0):
         for i in range(divisor):
-            divided -= 1
-            if(divided < 0):
+            dividend -= 1
+            if(dividend < 0):
                 return count
-        count += 1
+        count += add
     return count
 
 if __name__ == "__main__":
-    print(divide(4,3))
+    print(divide(-4,3))
