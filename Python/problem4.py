f3769253a7c1343a72de96f4f3683c21f994ca8c
aidanmc95 <aidanmc95@gmail.com>
2020-11-28 23:34:35 -0800

    Version 1

--- Python/problem4.py
+++ Python/problem4.py
@@ -1,6 +1,20 @@
 array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 index = 3
 
-del array[index]
+def removeDuplicates(self, nums: List[int]) -> int:
+        spot = 1
+        check = nums[0]
+        length = len(nums)
+        while(spot < length):
+            if(check == nums[spot]):
+                del nums[spot - 1]
+                length = len(nums)
+            else:
+                check = nums[spot - 1]
+                spot += 1
+            print(nums)
+            print(spot, check, length)
+        print(nums)
+        return len(nums)
 
 print array
