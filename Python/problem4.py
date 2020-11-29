45612596f5a80463b59bc2bc698c799f5fdb3ee4
aidanmc95 <aidanmc95@gmail.com>
2020-11-28 23:39:42 -0800

    Return 0 if empty list)

--- Python/problem4.py
+++ Python/problem4.py
@@ -2,19 +2,20 @@ array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 index = 3
 
 def removeDuplicates(self, nums: List[int]) -> int:
-        spot = 1
-        check = nums[0]
-        length = len(nums)
-        while(spot < length):
-            if(check == nums[spot]):
-                del nums[spot - 1]
-                length = len(nums)
-            else:
-                check = nums[spot - 1]
-                spot += 1
-            print(nums)
-            print(spot, check, length)
+    if (len(nums) == 0) return 0;
+    spot = 1
+    check = nums[0]
+    length = len(nums)
+    while(spot < length):
+        if(check == nums[spot]):
+            del nums[spot - 1]
+            length = len(nums)
+        else:
+            check = nums[spot - 1]
+            spot += 1
         print(nums)
-        return len(nums)
+        print(spot, check, length)
+    print(nums)
+    return len(nums)
 
 print array
